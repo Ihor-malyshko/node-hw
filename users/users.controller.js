9ed769062154310ccdfd6648adffdd86ceff620b
ihor <malishkoio@gmail.com>
2020-11-26 18:49:35 +0200

    add updateUsersSubscription

--- users/users.controller.js
+++ users/users.controller.js
@@ -1,5 +1,21 @@
 const { serializeUser } = require("./users.serializer");
+const { UserModel } = require("./users.model");
 
 exports.getCurrentUser = (req, res, next) => {
   res.status(200).send(serializeUser(req.user));
+};
+
+exports.updateUsersSubscription = async (req, res, next) => {
+  try {
+    const { id } = req.user;
+    const updatedUser = await UserModel.findByIdAndUpdate(id, req.body, {
+      new: true,
+    });
+    if (!updatedUser) {
+      return res.status(404).send("User not found");
+    }
+    return res.status(200).send(serializeUser(updatedUser));
+  } catch (err) {
+    next(err);
+  }
 };
--- users/users.router.js
+++ users/users.router.js
@@ -1,9 +1,20 @@
 const { Router } = require("express");
+const { validate } = require("../helpers/validate.middleware");
 const { authorize } = require("../helpers/authorize.middeleware");
-const { getCurrentUser } = require("./users.controller");
+const {
+  getCurrentUser,
+  updateUsersSubscription,
+} = require("./users.controller");
+const { updateUsersSubscriptionSchema } = require("./users.schemes");
 
 const router = Router();
 
 router.get("/current", authorize, getCurrentUser);
+router.patch(
+  "/",
+  authorize,
+  validate(updateUsersSubscriptionSchema),
+  updateUsersSubscription
+);
 
 exports.usersRouter = router;
